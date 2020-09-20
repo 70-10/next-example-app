7042c8f83c8868c0786db51a54b3f10f2d2c2688
70_10 <7010.dev@gmail.com>
2020-09-20 17:35:32 +0900

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,5 @@
 import React from "react";
-import Head from "next/head";
-import Top from "../compositions/top";
 
-type Props = {
-  title: string;
-};
+const Top = () => <h1>Hello</h1>;
 
-class App extends React.Component<Props> {
-  static async getInitialProps(): Promise<Props> {
-    return { title: "Hello world" };
-  }
-  render() {
-    return (
-      <>
-        <Head>
-          <title>{this.props.title}</title>
-        </Head>
-        <Top />
-      </>
-    );
-  }
-}
-
-export default App;
+export default Top;
